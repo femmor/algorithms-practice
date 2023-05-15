600acb301205310e11308de60acb300a79317dba
femmor <fegomson@gmail.com>
2023-05-15 16:14:20 -0500

    absolute value sum minimize and add algo added

--- 100-algo-challenge/absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ 100-algo-challenge/absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,12 @@
 export function absoluteValuesSumMinimization(a: number[]): number {
-    return 5;
+  // Check if the array is even or odd
+  const isEven = a.length % 2 === 0;
+  // If even, return the middle number
+  return isEven ? a[a.length / 2 - 1] : a[Math.floor(a.length / 2)];
+  // If odd, return the middle number
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
--- 100-algo-challenge/add/add.ts
+++ 100-algo-challenge/add/add.ts
@@ -1,9 +1,15 @@
 export function add(param1: number, param2: number): number {
-
+  return param1 + param2;
 }
 
 export function add2(...param1: number[]): number {
+  let result = 0;
 
+  param1.forEach((num) => {
+    result += num;
+  });
+
+  return result;
 }
 // console.log(add(1, 2));
 // console.log(add(3, 2));
