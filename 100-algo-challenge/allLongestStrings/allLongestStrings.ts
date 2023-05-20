4d62009a099fb770aa2cd205087bf5155c4c5048
femmor <fegomson@gmail.com>
2023-05-20 03:02:08 -0500

    Solved allLongestStrings

--- 100-algo-challenge/allLongestStrings/allLongestStrings.ts
+++ 100-algo-challenge/allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,21 @@
 export function allLongestStrings(inputArray: string[]): string[] {
+  const newArr: string[] = [];
+  let maxLength = 0;
 
+  // Find the maxLength
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i].length > maxLength) {
+      maxLength = inputArray[i].length;
+    }
+  }
+
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i].length >= maxLength) {
+      newArr.push(inputArray[i]);
+    }
+  }
+
+  return newArr;
 }
 
-console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+console.log(allLongestStrings(['aba', 'aa', 'ad', 'vcd', 'aba']));
